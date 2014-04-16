23dd6225ccee25c4a5f2d7c87525cf4a6c6c1231
alorenzana <lc_toni@MacBook-Pro-de-Antoni.local>
2014-04-16 17:44:53 +0200

    View 1
    view1 casi casi echa

--- client/views/view1/view1.js
+++ client/views/view1/view1.js
@@ -1,15 +1,9 @@
-Template.view1.greeting = function () {
-  return "Welcome to myfirstapp.";
-};
-
 Template.view1.events({
   'click input': function () {
     // template data, if any, is available in 'this'
     if (typeof console !== 'undefined')
       console.log("You pressed the button");
-    
-    Session.set("currentPage", "view2");
-    
+  	  Session.set("currentPage", "view2");
   }
 });
 
