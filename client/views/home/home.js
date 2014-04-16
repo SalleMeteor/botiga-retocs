169992891802fa124ded428351a28ff6a54c5de3
alorenzana <lc_toni@MacBook-Pro-de-Antoni.local>
2014-04-16 15:53:34 +0200

    Esqueleto

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -4,6 +4,26 @@ Template.home.created = function () {
   Meteor.subscribe("allContacts");
 }
 
+Template.view1.events({
+  'click1 #button': function () {
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+      console.log("You pressed the button 1");
+    
+    Session.set("currentPage", "view1");
+  }
+  'click2 #button': function () {
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+      console.log("You pressed the button 2");
+    
+    Session.set("currentPage", "view2");
+  }
+  
+});
+
+
+
 Template.home.helpers({
   page: function(page) {
     return Session.equals("currentPage", page);     
